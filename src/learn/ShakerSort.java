5e9755e2ae37b50b97e7956becb399f504fb262d
Marco Mantovani <marcomanto99@gmail.com>
2017-05-02 12:46:15 -0700

    Modifica

---
+++ src/learn/ShakerSort.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package learn;
+/**
+ *
+ * @author mantovanimarco
+ */
+public class ShakerSort {
+    
+    public static int[] sort( int[] array ){
+    boolean swapped;
+    do {
+      swapped = false;
+      for (int i =0; i<=  array.length  - 2;i++) {
+        if (array[ i ] > array[ i + 1 ]) {
+          int temp = array[i];
+          array[i] = array[i+1];
+          array[i+1]=temp;
+          swapped = true;
+        }
+      }
+      if (!swapped) {
+        break;
+      }
+      swapped = false;
+      for (int i= array.length - 2;i>=0;i--) {
+        if (array[ i ] > array[ i + 1 ]) {
+          int temp = array[i];
+          array[i] = array[i+1];
+          array[i+1]=temp;
+          swapped = true;
+        }
+      }
+    } while (swapped);
+    return array;//
+  }
+}
+
