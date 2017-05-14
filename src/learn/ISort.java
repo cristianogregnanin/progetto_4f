ccd4ba31df0b3d0ce5e6fc4a52005a8e7d4e1b96
PaulonLuca <paulon.luca@gmail.com>
2017-05-14 11:22:26 +0200

    Progetto terminato

---
+++ src/learn/ISort.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package learn;
+
+/**
+ *
+ * @author lucapaulon
+ */
+public interface ISort {
+    public static void sort(int[] v)
+    {       }
+}
