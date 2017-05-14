70649c811dba35788f78615b0745162aa616ddd0
Emanuele-Stocco <stoccoemanuele@alice.it>
2017-05-14 21:09:24 +0200

    Add files via upload

---
+++ src/learn/ISort.java
@@ -0,0 +1,19 @@
+package learn;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author emanuele98
+ */
+public interface ISort 
+{
+    public static void Sort(int[] vettore)
+    {
+        
+    }
+}
