8b5415874f7ec7e83c29294c3fc77f22afcdc3dd
Emanuele-Stocco <stoccoemanuele@alice.it>
2017-05-14 21:33:02 +0200

    Add files via upload

---
+++ test/TestElaboraStringa.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package learn;
+/*package org;
+import static org.junit.Assert.assertEquals;
+import org.junit;
+import org.junit.Test;*/
+/**
+ *
+ * @author emanuele98
+ */
+public class TestElaboraStringa 
+{
+    /*@Test
+    public void TestStringa()
+    {
+        SortComparationGUI sort = new SortComparationGUI();
+        String st = sort.ElaboraStringa("Selection Sort");
+        assertEquals("learn.SelectionSort", st);
+    }*/
+}
